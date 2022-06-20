9615a74f6ce13cda332167abe234c46d35b500e3
yehuditCohen020302 <yehudit8824@gmail.com>
2022-06-20 11:43:58 +0300

    push 20/06

--- bootcamp-template-main/src/manager.js
+++ bootcamp-template-main/src/manager.js
@@ -1,14 +1,16 @@
  class Manager{
 
+    oReq = new XMLHttpRequest()
+
     constructor(firstName, lastName, city,street, houseNumber,phoneNumber,emailAddress){
     
         this.firstName   =firstName,
         this.lastName    =lastName,
-        this.city        =city,
-        this.street      =street,
-        this.houseNumber =houseNumber,
-        this.phoneNumber =phoneNumber,
         this.emailAddress=emailAddress
+    }
+
+    search(text) {
+        this.oReq("get", )
     }
 
     login(){
@@ -21,16 +23,20 @@
     OKaddUser(){
 
         //create new user object
-        var user= new User(document.getElementById("firstName").value,
+        let user= new User(document.getElementById("firstName").value,
                             document.getElementById("lastName").value,
                             document.getElementById("city").value,
                             document.getElementById("street").value,
                             document.getElementById("houseNumber").value,
                             document.getElementById("phoneNumber").value,
-                            document.getElementById("emailAddress").value );
+                            document.getElementById("emailAddress").value,
+                            document.getElementById("height").value,
+                            document.getElementById("beginingWeight").value );
 
+        // JSON.stringify(user);
+        console.log(user);
         //and push the new user to the json file...
-         sessionStorage.setItem("currentUser",user);
+         sessionStorage.setItem("currentUser",user); //JSON.parse(user)
     }
 
 }
