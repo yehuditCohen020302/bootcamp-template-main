294259ad546d8667a279018717918ca20a68ef4e
michal <859michal@gmail.com>
2022-06-30 11:17:07 +0300

    small changes

--- bootcamp-template-main/src/user.js
+++ bootcamp-template-main/src/user.js
@@ -12,7 +12,8 @@
         this.phoneNumber    =phoneNumber,
         this.emailAddress   =emailAddress,
         this.height         =height,
-        this.addMeeting(beginingWeight)
+        this.addMeeting(beginingWeight),
+        this.diary=[]
     }
     addMeeting(newWeight){
         var currentWeight=new meeting(new Date(),newWeight)
--- bootcamp-template-main/src/userDiary.js
+++ bootcamp-template-main/src/userDiary.js
@@ -1,5 +1,4 @@
 var allFoods=[];
-var hasAlreadyDiary=true;
 var currentUser;
 
 function showUserDiary(){
@@ -19,7 +18,6 @@ function getByEmail(emailAddress){
             drawUserDiary(response[0])
     })
     .catch(err => {
-        hasAlreadyDiary=false;
         console.log(err)})
 }
 
@@ -32,61 +30,49 @@ function drawUserDiary(currentUser) {
 
   
 function saveChanges(){
-    if(hasAlreadyDiary){
-        updateUsersDiary()
-    }else{
-        addNewUsersDiary();
-    }
-}
-
-function updateUsersDiary(){
+  var today=currentUser.diary.filter(day=>day.date==Date.now())
     
-}
-
-function addNewUsersDiary(){
-    var today=currentUser.diary.filter(day=>day.date==Date.now())
-    
-const requestOptions = {
-    method: 'PUT',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ title: 'Fetch PUT Request Example' })
-};
-fetch('https://reqres.in/api/articles/1', requestOptions)
-    .then(response => response.json())
-    .then(data => element.innerHTML = data.updatedAt );
-
-      fetch('http://localhost:3000/users', {
-        method: "POST",
-        body: JSON.stringify(_data),
-        headers: {"Content-type": "application/json; charset=UTF-8"}
-      })
-      .then(response => response.json()) 
-      .then(json => console.log(json))
-      .catch((err) => console.log(err));
-}
-
-
-function addDate(){
-    var modal = document.getElementById("myModal");
-    // When the user clicks on the button, open the modal
-    btn.onclick = function() {
-      modal.style.display = "block";
-    }
-    // Get the button that opens the modal
-    var btn = document.getElementById("myBtn");
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-      modal.style.display = "none";
-    }
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-      if (event.target == modal) {
+  const requestOptions = {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ title: 'Fetch PUT Request Example' })
+  };
+  fetch('https://reqres.in/api/articles/1', requestOptions)
+      .then(response => response.json())
+      .then(data => element.innerHTML = data.updatedAt );
+  
+        fetch('http://localhost:3000/users', {
+          method: "POST",
+          body: JSON.stringify(_data),
+          headers: {"Content-type": "application/json; charset=UTF-8"}
+        })
+        .then(response => response.json()) 
+        .then(json => console.log(json))
+        .catch((err) => console.log(err));
+  }
+  
+  
+  function addDate(){
+      var modal = document.getElementById("myModal");
+      // When the user clicks on the button, open the modal
+      btn.onclick = function() {
+        modal.style.display = "block";
+      }
+      // Get the button that opens the modal
+      var btn = document.getElementById("myBtn");
+      // Get the <span> element that closes the modal
+      var span = document.getElementsByClassName("close")[0];
+      // When the user clicks on <span> (x), close the modal
+      span.onclick = function() {
         modal.style.display = "none";
       }
-    }
- }
+      // When the user clicks anywhere outside of the modal, close it
+      window.onclick = function(event) {
+        if (event.target == modal) {
+          modal.style.display = "none";
+        }
+      }
+}
 
   
 
