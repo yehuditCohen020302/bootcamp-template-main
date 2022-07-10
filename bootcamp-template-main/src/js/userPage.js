695399f0914df2320c5055645cad586a324dd8c8
yehuditCohen020302 <yehudit8824@gmail.com>
2022-07-10 18:01:39 +0300

    add swagger

--- bootcamp-template-main/src/js/userPage.js
+++ bootcamp-template-main/src/js/userPage.js
@@ -1,6 +1,6 @@
 
 function loadUser(){
-    // debugger
+    debugger
     const params = new URLSearchParams(window.location.search)
     if(params.has("userId"))
         getUserById(params.get("userId"));
@@ -40,7 +40,7 @@ function getUserByEmail(emailAddress) {
 }
 
 function drawUserDetails(currentUser) {
-    // debugger
+    debugger
     document.getElementById("userId").value=currentUser.id;
     document.getElementById("firstName").value=currentUser.firstName;
     document.getElementById("lastName").value=currentUser.lastName;
