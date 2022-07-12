910ccfdbebd96fced4d3bd9d6bf88df2857bbb9f
michal <859michal@gmail.com>
2022-07-12 17:56:19 +0300

    to Yehudit

--- bootcamp-template-main/src/js/userDiary.js
+++ bootcamp-template-main/src/js/userDiary.js
@@ -1,33 +1,31 @@
-const baseUrl = "https://pacific-headland-08901.herokuapp.com/";
+const baseUrl = 'https://pacific-headland-08901.herokuapp.com/';
 let allFoods=[];
-let currentUser;
+let currentUserId;
+let currentUserDiary;
 
 function showUserDiary(){
-    console.log("UserDiary: in showUserDiary");
+    console.log('UserDiary: in showUserDiary');
     const params=new URLSearchParams(window.location.search);
-    getByEmail(params.get("emailAddress"));
+    currentUserId=params.get('userId');
+    getByUserId();
 }
 
-function getByEmail(emailAddress){
-    fetch(baseUrl +'users')
+function getByUserId(){
+    fetch(`${baseUrl}users/${currentUserId}/diary`)
     .then(response => response.json())
-    .then(response => response.filter(user=>user.emailAddress === emailAddress))
     .then(response=>{
-            currentUser=response[0];
+            currentUserDiary=response;
             drawUserDiary()
-
     })
     .catch(err => {
         console.log(err)})
 }
 
 function drawUserDiary() {
-    document.getElementById("nameUser").innerHTML=currentUser.firstName+" "+ currentUser.lastName;
-
-    currentUser.diary.days.forEach(day=>{
-      const tmp=document.getElementsByTagName("template")[0];
+    currentUserDiary.days.forEach(day=>{
+      const tmp=document.getElementsByTagName('template')[0];
       let element=tmp.content.cloneNode(true);
-      element.querySelector(".date").innerText=day.date;
+      element.querySelector('.date').innerText=day.date;
       let mealNumber=1;
       day.meals.forEach(meal=>{
         element.querySelector(`.meal${mealNumber}`).innerText=meal.toString();
@@ -37,22 +35,24 @@ function drawUserDiary() {
       mealsTable.append(element);
     });
 }
-
   
 function save(){
   let foodList=[];
   let numOfFood=1;
-  while(document.getElementById(numOfFood)!=undefined && document.getElementById(numOfFood).value!=""){
+  while(document.getElementById(numOfFood)!=undefined && document.getElementById(numOfFood).value!=''){
     foodList.push(document.getElementById(numOfFood).value)
     numOfFood++;
   }
   let todayDiary;
   let today=document.querySelector('.dateOfMeal').value;
-  let findTodayDiary=currentUser.diary.days.filter(day=>day.date==today);
-  if(findTodayDiary.length>0) {
+  let findTodayDiary
+  if(currentUserDiary.days)
+    findTodayDiary = currentUserDiary.days.filter(day=>day.date==today);
+  let dateId='';
+  if(findTodayDiary) {
     todayDiary=findTodayDiary[0];
     todayDiary.meals.push(foodList);
-    currentUser.diary.days=currentUser.diary.days.filter(day=>day.date!=today);
+    dateId=`${today[0]}${today[1]}${today[2]}${today[3]}${today[5]}${today[6]}${today[8]}${today[9]}`
   }else{
     todayDiary={
       date:today,
@@ -61,62 +61,59 @@ function save(){
       ]
     }
   }
-  currentUser.diary.days.push(todayDiary);
-  
-    fetch(`${baseUrl}users/${currentUser.id}`, {
-      method: "put",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(currentUser)
-    }).then(response => {
-      if(response.ok)
-        console.log("update the user's diary run successfully!!")
-      else
-        console.log(response.status)
-    }).catch(err => console.log(err));
-   
-    showUserDiary()
-    
+  fetch(`${baseUrl}users/${currentUserId}/diary/${dateId}`, {
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(todayDiary)
+  }).then(response => {
+    if(response.ok)
+      console.log("update the user's diary run successfully!!")
+    else
+      console.log(response.status)
+  }).catch(err => console.log(err));
+  document.location.reload(true);
+    // showUserDiary()
 }
 
 
 function drowMeal(){
  
-  const element = document.querySelector(".add-meal-card");
+  const element = document.querySelector('.add-meal-card');
   const cln = element.content.cloneNode(true);
-  cln.querySelector(".meal-title").innerText = `new meal`;
-  document.querySelector(".modal-content").appendChild(cln);
+  cln.querySelector('.meal-title').innerText = `new meal`;
+  document.querySelector('.modal-content').appendChild(cln);
 }
 let foodNumber=2;
 function addMoreFood(){
-  let container = document.getElementById("newMealContainer");
-  let input = document.createElement("input");
-  input.type = "text";
+  let container = document.getElementById('newMealContainer');
+  let input = document.createElement('input');
+  input.type = 'text';
   input.id = foodNumber;
-  input.value="";
-  input.placeholder="Enter food";
+  input.value='';
+  input.placeholder='Enter food';
   container.appendChild(input);
-  container.appendChild(document.createElement("br"));
-  container.appendChild(document.createElement("br"));
+  container.appendChild(document.createElement('br'));
+  container.appendChild(document.createElement('br'));
   foodNumber++;
 }
 
 
 addMeal=()=>{
-    document.querySelector(".dateOfMeal").value = new Date().toISOString().split('T')[0];
-    modal = document.getElementById("myModal");
-    let btn = document.getElementById("myBtn");
-    let span = document.getElementsByClassName("close")[0];
+    document.querySelector('.dateOfMeal').value = new Date().toISOString().split('T')[0];
+    modal = document.getElementById('myModal');
+    let btn = document.getElementById('myBtn');
+    let span = document.getElementsByClassName('close')[0];
     btn.onclick = function() {
-      modal.style.display = "block";
+      modal.style.display = 'block';
     }
     span.onclick = function() {
-      modal.style.display = "none";
+      modal.style.display = 'none';
     }
     window.onclick = function(event) {
       if (event.target == modal) {
-        modal.style.display = "none";
+        modal.style.display = 'none';
       }
     }
 }
